c1beaa133939253217419c926d08749b08d79f63
Tim Saunders <tim@sauntimo.org>
2018-10-24 03:07:17 +0100

    config > prod.env: removed vbls

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -2,7 +2,4 @@
 
 module.exports = {
     NODE_ENV       : '"production"'
-    DB_USERNAME    : 'sauntimo_sto_admin',
-    DB_PASSWORD    : 'O&L#9L#XAK38l679Py%rU0Gz^BKcxl',
-    LASTFM_API_KEY : '7532ac8c9a8cf7db9bf4fe7362e73cc3'
 }
